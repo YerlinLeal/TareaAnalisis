88cbecb01cfcc9c71d8bd04eb7ed07ea503d9097
YerlinLeal <yerlin.leal@ucr.ac.cr>
2022-11-12 23:15:52 -0600

    Metodos del modelo iterador

--- src/domain/ArrayList.java
+++ src/domain/ArrayList.java
@@ -9,30 +9,39 @@ public class ArrayList implements Iterable, List{
     public ArrayList(){
         this.elementos = new Object[this.totalElementos];
         this.contador = 0;
-    }
+    } // constructor
 
     @Override
     public Iterator createIterator() {
-        return null;
+        return new ArrayListIterator(this);
     }
 
     @Override
     public boolean add(Object object) {
+        if(this.contador <= this.totalElementos){
+            this.elementos[contador] = object;
+            contador++;
+            return true;
+        }
         return false;
     }
 
     @Override
     public Object get(int i) {
-        return null;
+        return this.elementos[i];
     }
 
     @Override
     public boolean isEmpty() {
+        if (this.contador == 0){
+            return true;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.contador;
     }
+
 }
